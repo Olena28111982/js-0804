c7ca179381ee876fff339051cd2f490dee65c1db
Koval Olena <elenkoval2811gmail.com>
2024-04-11 23:06:31 +0300

    Inisial js

---
+++ index.js
@@ -0,0 +1,80 @@
+// alert("ten");
+// alert(77);
+// alert(true);
+
+// console.log(10, 15, "ten");
+
+// const userName = "Маша";
+// console.log("userName =" , userName);
+
+// const userAge = 23;
+// console.log("userAge =" , userAge);
+
+// let ageAnimal;
+// console.log('ageAnimal = ', ageAnimal);
+// console.log('type ageAnimal = ', typeof ageAnimal);
+// const amountPawsAnimal = 4;
+
+// const login  = 'qwerty';
+// {
+
+//   const login = 'erty' 
+// }
+// console.log('login = ', login);
+
+
+
+
+// const airTemperatureCelsius = 24;
+
+// let namebook;
+
+// const lengthRectangle= 3;
+// const widthRectangle = 4;
+// const result = lengthRectangle * widthRectangle;
+// console.log('result =', result);
+
+// const isFigureTriangle = false;
+
+// const elementSize=null;
+
+
+// let number1 = 10;
+// const number2 = 4;
+// number1 + = number2;
+
+// number1 = number1 + number2;
+
+// number1 + = number2;
+
+// number1 = number1 / 3;
+// number1 / = 3;
+
+// number1 = number1 * number2;
+// number1 * = number2;
+
+// number1 = number1 ** 2;
+// number1 ** = 2;
+
+// number1 = number1 % 5;
+// number1 % = 5;
+
+
+1.
+const amountBooksLibrary = 50;
+const amountReadBooksLibrary=10;
+const result1 = amountReadBooksLibrary/amountBooksLibrary*100;
+console.log('result1=', result1 ); 
+
+2.
+let walletBalanceUah = 1000;
+const exchangeRateEuroToUah = 42.09;
+walletBalanceUah /= exchangeRateEuroToUah;
+console.log('walletBalanceUah=', walletBalanceUah);
+
+3.
+const costMeatSteak =300;
+const costSalad= 200;
+const costDrink=80;
+const result2 = (costMeatSteak + costSalad + costDrink)*115/100;
+console.log('result2 = ', result2);
